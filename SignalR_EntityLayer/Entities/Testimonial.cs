572e05c805eb28c4341abc2f49ad12d71e7380cf
Ali Can Dogan <76850300+doganalican46@users.noreply.github.com>
2023-11-09 11:39:05 +0300

    entities are added

---
+++ SignalR_EntityLayer/Entities/Testimonial.cs
@@ -0,0 +1,15 @@
+﻿namespace SignalR_EntityLayer.Entities
+{
+    class Testimonial
+    {
+        public int TestimonialID { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+
+        public string Comment { get; set; }
+        public string ImageUrl { get; set; }
+        public bool TestimonialStatus { get; set; }
+
+
+    }
+}
